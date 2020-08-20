ecae99aeb9a7f2ae84b783890bc3789e8552c311
Christian <christian@chfi.se>
2020-08-19 19:36:41 -0500

    Generating matrices

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,34 @@
 mod utils;
 
+use rand::prelude::*;
+
+use nalgebra as na;
+
+use nalgebra::{DMatrix, DVector, Matrix, Unit};
+
 use wasm_bindgen::prelude::*;
+
+fn rotation_mat(dim: usize, angle: f32, a: usize, b: usize) -> DMatrix<f32> {
+    assert!(a < dim && b < dim && a != b);
+    let mut mat = DMatrix::identity(dim, dim);
+    mat[(a, a)] = angle.cos();
+    mat[(a, b)] = -angle.sin();
+    mat[(b, a)] = angle.sin();
+    mat[(b, b)] = angle.cos();
+    mat
+}
+
+fn rand_rot_mat(dim: usize) -> DMatrix<f32> {
+    let mut rng = thread_rng();
+    let angle: f32 = rng.gen();
+    let a: usize = rng.gen_range(0, dim);
+    let mut b: usize = rng.gen_range(0, dim);
+    while a == b {
+        b = rng.gen_range(0, dim);
+    }
+
+    rotation_mat(dim, angle, a, b)
+}
 
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
 // allocator.
@@ -9,7 +37,7 @@ use wasm_bindgen::prelude::*;
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 #[wasm_bindgen]
-extern {
+extern "C" {
     fn alert(s: &str);
 }
 
